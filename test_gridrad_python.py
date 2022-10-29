11d5fc6829a345ba729b7f05aeb46016706b5d84
Tim Supinie <tsupinie@gmail.com>
2022-10-29 16:59:35 -0500

    Move everything to __init__.py

--- test_gridrad_python.py
+++ test_gridrad_python.py
@@ -1,8 +1,8 @@
-import gridradpy.gridrad as gridrad
+import gridradpy
 
 fname = '/Users/tsupinie/data/gridrad/nexrad_3d_v4_2_20110426T220000Z.nc'
 
-data_xr = gridrad.read_file(fname)
-data_xr = gridrad.filter(data_xr)
-data_xr = gridrad.remove_clutter(data_xr, skip_weak_ll_echo=True)
-plot = gridrad.plot_image(data_xr, 'gridrad_image_xr.png')
+data_xr = gridradpy.read_file(fname)
+data_xr = gridradpy.filter(data_xr)
+data_xr = gridradpy.remove_clutter(data_xr, skip_weak_ll_echo=True)
+plot = gridradpy.plot_image(data_xr, 'gridrad_image_xr.png')
